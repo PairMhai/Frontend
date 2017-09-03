bc6f814dc6f994d855e5b1e67303d373aa674f84
Thanawan <monika-amp@hotmail.com>
2017-09-04 00:43:02 +0700

    add filter

--- src/components/LeftTab.js
+++ src/components/LeftTab.js
@@ -7,28 +7,46 @@ class LeftTab extends Component {
             <div>
                 <div className="left-tab">
                 <br></br>
-                   <a>&nbsp;&nbsp;SORT BY :</a>
+                <br></br>
+                   <a className="head">&nbsp;&nbsp;SORT BY :</a>
                    <br></br>
-                   <br></br>
+                   
                    &nbsp;&nbsp;<input type="radio" name="radio-btn" />&nbsp;&nbsp;PRICE
                    &nbsp;&nbsp;<input type="radio" name="radio-btn" />&nbsp;&nbsp;ALPHABET
                    <br></br>
                    <br></br>
                     <form action="" method="get" class="search">
 
-                    <div class="form__field">
-                        &nbsp;&nbsp;<input type="search" name="search" placeholder="Enter here" class="form__input"></input>
-                        <input type="submit" value="Search" class="button"></input>
+                    <div className="form__field">
+                        &nbsp;&nbsp;<input className="searchbar" type="search" name="search" placeholder="Enter here" class="form__input"></input>
+                        <input className="buttonsearch" type="submit" value="Search" class="button"></input>
                   
                     </div>
                     </form>
                     <br></br>
-                   <a>&nbsp;&nbsp;PRICE RANGE :</a>
+                   <div>
+                    <a className="head">&nbsp;&nbsp;&nbsp;PRICE RANGE :&nbsp;</a>
                    <br></br>
-                   <br></br>
+                   <select className="classic2" id="pricerange" onchange="">
+                        <option value="pricer1">&nbsp; 1000-2000 </option>
+                        <option value="pricer2">&nbsp; 2000-5000</option>
+                    </select>
+                    </div>
+                    <br></br>
+                    <a className="head" >&nbsp;&nbsp;&nbsp;COLOR :&nbsp;</a>
+                    <br></br>
+
+                    <br></br>                                        
+                    <div>
+                    <a className="head">&nbsp;&nbsp;&nbsp;MATERIAL :&nbsp;</a>
                    
+                   &nbsp;<select className="classic2" id="pricerange" onchange="">
+                        <option value="Mudmee Silk">&nbsp; Mudmee Silk </option>
+                        <option value="normal silk">&nbsp; Normal Silk</option>
+                    </select>
+                    </div>
 
-
+                    
                 
                 </div>
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,23 @@ class Navbar extends Component {
  
       render(){
         return (
-            <div>
-              <div className="nav-container">
-             <img src="../src/img/logomain.png" alt="Error" width="300" height="100"  />
-                <a className="btn" href="/material" role="button">Material</a>
-                <a className="btn" href="/designed" role="button">Designed</a>
-                <a className="btn" href="/info" role="button">Thai Silk's History</a>
-                <a className="btn" href="/contact" role="button">Contact Us</a>
-              </div>
-             
-              
+          
+          <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
             </div>
+            <ul class="nav navbar-nav">
+            <img src="../src/img/logomain.png" alt="Error" width="300" height="100"  />
+            <a className="btn btn-primary btn-lg sharp" href="/material" role="button">Material</a>
+            <a className="btn btn-primary btn-lg sharp" href="/designed" role="button">Designed</a>
+            <a className="btn btn-primary btn-lg sharp" href="/info" role="button">Thai Silk's History</a>
+            <a className="btn btn-primary btn-lg sharp" href="/contact" role="button">Contact Us</a>
+            </ul>
+            
+          </div>
+        </nav>
+
+            
         );
     }
 }
