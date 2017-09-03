2f12c942a5d47c78718cf09068ce3d6340d0821c
Thanawan <monika-amp@hotmail.com>
2017-09-03 14:16:32 +0700

    made a change

--- src/components/LeftTab.js
+++ src/components/LeftTab.js
@@ -1,14 +1,39 @@
 import React , {Component} from 'react'
 
 class LeftTab extends Component {
-
+   
     render(){
         return (
             <div>
                 <div className="left-tab">
-                   <button>kkk</button>
+                <br></br>
+                   <a>&nbsp;&nbsp;SORT BY :</a>
+                   <br></br>
+                   <br></br>
+                   &nbsp;&nbsp;<input type="radio" name="radio-btn" />&nbsp;&nbsp;PRICE
+                   &nbsp;&nbsp;<input type="radio" name="radio-btn" />&nbsp;&nbsp;ALPHABET
+                   <br></br>
+                   <br></br>
+                    <form action="" method="get" class="search">
+
+                    <div class="form__field">
+                        &nbsp;&nbsp;<input type="search" name="search" placeholder="Enter here" class="form__input"></input>
+                        <input type="submit" value="Search" class="button"></input>
+                  
+                    </div>
+                    </form>
+                    <br></br>
+                   <a>&nbsp;&nbsp;PRICE RANGE :</a>
+                   <br></br>
+                   <br></br>
+                   
+
+
+                
                 </div>
-            </div>
+                </div>
+
+                
         );
     }
 }
