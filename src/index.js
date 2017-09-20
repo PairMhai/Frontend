33186de87c153b4194e39064a69cc7de395924f5
Thanawan <monika-amp@hotmail.com>
2017-09-20 19:54:49 +0700

    slide

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+
 import Highlight from './pages/Highlight'
 import Home from './pages/Home'
 import Material from './pages/Material'
--- src/pages/Highlight.js
+++ src/pages/Highlight.js
@@ -4,43 +4,46 @@ import '../CSS/Highlight.css';
 class Highlight extends Component {
 
     render(){
+        
+
         return (
-            <div>
-                <div className="col-lg-6"> 
-                    <img src={ require('../img/logomain.png') } width="100%" height="100%"/>
-                    <a className="btn btn-primary" href="/home" role="button">Home</a>
-                </div>
-               
-                <div className="col-lg-6">
-                    <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                        <ol className="carousel-indicators">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                        </ol>
-                        <div className="carousel-inner" role="listbox">
-                            <div className="carousel-item active">
-                                <img className="d-block img-fluid" src={ require('../img/logomain.png') } alt="First slide"/>
-                            </div>
-                            <div className="carousel-item">
-                                <img className="d-block img-fluid" src={ require('../img/logomain.png') } alt="Second slide"/>
-                            </div>
-                            <div className="carousel-item">
-                                <img className="d-block img-fluid" src={ require('../img/logomain.png') } alt="Third slide"/>
-                            </div>
-                        </div>
-                        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Next</span>
-                        </a>
-                    </div>
-                </div>
-                
+            
+            <div className="row container-fluid">
+            <div className="hl-content-logo"> 
+                <img src={ require('../img/logomain.png') } width="80%"/>
+                <a className="btn btn-primary" href="/home" role="button">Home</a>
             </div>
+           
+            <div className="hl-content-promotion">
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                        <li data-target="#carouselExample" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIn" data-slide-to="2"></li>
+                    </ol>
+                    <div className="carousel-inner" role="listbox">
+                        <div className="carousel-item active">
+                            <img className="d-block img-fluid" width="100%" height="100%" src={ require('../img/promotion1.jpg') } alt="First slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block img-fluid" width="100%" height="100%" src={ require('../img/promotion2.jpg') } alt="Second slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block img-fluid" width="100%" height="100%" src={ require('../img/logomain.png') } alt="Third slide"/>
+                        </div>
+                    </div>
+                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            
+        </div>
         );
     }
 }
