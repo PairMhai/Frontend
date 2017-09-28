d859d1f654636baf3dbcbff319a4a6363872081f
Thanawan <monika-amp@hotmail.com>
2017-09-29 00:59:06 +0700

    edit left tab

--- src/components/LeftTab.js
+++ src/components/LeftTab.js
@@ -5,18 +5,19 @@ class LeftTab extends Component {
    
     render(){
         return (
-            <div className="login-layout">
-                <section className="login-left">
+            <div>
+                <div className="login-left">
                    <p className="login-label">USERNAME: </p><br></br>  
                    <input type="user" name="user" className="login-input"></input><br></br>
                    <p className="login-label">PASSWORD: </p><br></br> 
                    <input type="pass" name="pass" className="login-input"></input><br></br>
                    <a href="#" className="forget">Forgot your password?</a><br></br> 
                    <input className="login-left-btn" type="submit" value="SIGN IN" />
-                  <br></br>
-                   ----------------or-----------------<br></br>
+                  <br/>
+                  <p className="login-label">-------or-----</p>
+                  <br/>
                    <input className="login-left-btn" type="submit" value="SIGN UP" />
-                </section>
+                </div>
             </div>
         );
     }
