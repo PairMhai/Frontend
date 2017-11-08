c1b4124ee54b5fdb8fe3ef8aa72b62a173198ed6
Soraya Saenna <bubblebitoey@gmail.com>
2017-11-08 14:26:01 +0700

    Can add card

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,3 @@
-
 import React , {Component} from 'react'
 import {Cookies} from 'react-cookie'
 import Navbar from '../components/Navbar'
@@ -22,13 +21,11 @@ class SignUp extends Component {
         super(props);
         this.state = { username: '', password: '', cfpassword: '', firstname: '', lastname: '', 
         gender: '',email: '', birthday: '', tel: '',  address: '', classes: '', isActive: false,
-        cardNumber: '', bank:'', ccv:'', cardHolder:'', exp:'', customer:'', cardDetail:[]};
+        cardNumber: '', ccv:'', cardHolder:'', exp:'', customer:'', cardDetail:[]};
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleCard(event) {
+        this.handleAdd = this.handleAdd.bind(this);
     }
 
 
@@ -42,14 +39,16 @@ class SignUp extends Component {
         });
     }
 
+    handleAdd() {
+        const newCard = this.state.cardDetail;
+        newCard.push({owner: this.state.cardHolder, credit_no: this.state.cardNumber, ccv: this.state.ccv, expire_date: this.state.exp});
+        this.setState({cardDetail: newCard, isActive: !this.state.isActive});
+        console.log(this.state.cardDetail);
+
+        this.setState({ owner: '', credit_no: '', ccv: '', expire_date: '', cardHolder:''});
+    }
+
     componentWillMount() {
-        // var card_owner = this.state.card_holder;
-        // var card_number = this.state.card_number;
-        // var card_ccv = this.state.ccv;
-        // var card_expire = this.state.exp;
-
-        // this.setState({card_detail: card_owner, card_number, card_ccv, card_expire})
-
         Modal.setAppElement('body');
     }
 
@@ -58,22 +57,6 @@ class SignUp extends Component {
     }
 
     handleSubmit(event){
-        // axios.post('http://pairmhai-api.herokuapp.com/membership/register', {
-        //     "user": {
-        //         "username": this.state.username,
-        //         "first_name": this.state.firstname,
-        //         "last_name": this.state.lastname,
-        //         "email": this.state.email,
-        //         "telephone": this.state.tel,
-        //         "address": this.state.address,
-        //         "date_of_birth": this.state.birthday,
-        //         "gender": this.state.gender
-        //     },
-        //     "password1": this.state.password,
-        //     "password2": this.state.cfpassword,
-        //     "classes": this.state.classes
-        // })
-
         console.log("user name: " + this.state.username);
         console.log("first name: " + this.state.firstname);
         console.log("last name: " + this.state.lastname);
@@ -87,14 +70,6 @@ class SignUp extends Component {
         console.log("credit no: " + this.state.cardNumber);
         console.log("ccv: " + this.state.ccv);
         console.log("expire date: " + this.state.exp);
-
-        const newCard = this.state.card_detail;
-        newCard.push({cardOwner: this.state.cardHolder, cardNumber: this.state.cardNumber, cardCcv: this.state.ccv, cardExpire: this.state.exp});
-        this.setState({cardDetail: newCard});
-        console.log(this.state.cardDetail);
-
-        const cookies = new Cookies();
-        var key = cookies.set('cardDetail',this.state.cardDetail,);
 
 
         axios.post('https://pairmhai-api.herokuapp.com/membership/register/', {
@@ -112,46 +87,44 @@ class SignUp extends Component {
             "password2": this.state.cfpassword,
             "classes": this.state.classes,
 
-            "credit_cards": [
-                {
-                    "owner": this.state.cardHolder,
-                    "credit_no": this.state.cardNumber,
-                    "ccv": this.state.ccv,
-                    "expire_date": this.state.exp
-                },
-                {
-                    "owner": this.state.cardHolder,
-                    "credit_no": this.state.cardNumber,
-                    "ccv": this.state.ccv,
-                    "expire_date": this.state.exp
-                },
-            ]
+            "credit_cards": this.state.cardDetail,
         })
         .then(function (response) {
-            const cookies = new Cookies();
-            cookies.set('key', response.data.key, {path: '/'})
-            //window.location = "/home"
+            swal("Success","Please verify your e-mail", "success");
+            window.location = "/home"
             console.log(response);
         })
         .catch(function (error) {
-            console.log(error);
+            console.log(error.response);
             swal ( "Oops" ,  "Please enter valid data" ,  "error" )
         });
          event.preventDefault();  
-    }
+     }
 
   
     render() {
-        const allCard = this.state.cardDetail.map((cardVal, index)=>{
-           
-        });
+        // const allCard = this.state.cardDetail.map((cardVal, index)=>{
+        //   return 
+        //   <div>
+        //   <table className="payment-table">
+        //       <tbody>
+        //           <tr>
+        //               <td>4569 xxx xxx</td>
+        //               <td>TMB</td>
+        //               <td>3/20</td>
+        //               <td>Thanawan Sean-in</td>
+        //           </tr>
+        //       </tbody>
+        //   </table><br></br>
+        //   </div>
+        // });
         
         return (
             <div>
                 <Navbar />
             <div className="siup-center container-fluid">
                 <p className="signup">SIGN UP</p>
-                <form onSubmit={this.handleSubmit}>
+                
                     <div className="container">
                         USERNAME: <input type="user" name="username" value={this.state.username} onChange={this.handleChange}/>&nbsp;&nbsp;&nbsp;&nbsp;
                         PASSWORD: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>&nbsp;&nbsp;&nbsp;&nbsp;
@@ -220,7 +193,7 @@ class SignUp extends Component {
                         </table><br></br>
                         <div>
                             <button className="signup_btn pull-right" onClick={this.toggleModal}>ADD CARD</button>
-                            <Modal contentLabel="modal" isOpen={this.state.isActive} onRequestClose={this.toggleModal} contentLabel="Modal">
+                            <Modal contentLabel="modal" isOpen={this.state.isActive} onRequestClose={this.toggleModal}>
                                 <div>
                                     <p className="add-card-info">CARD INFORMATION</p>
                                 </div><br/>
@@ -233,20 +206,18 @@ class SignUp extends Component {
                                     </div>
                                     <br/>
                                     Card Number &nbsp;&nbsp;<input name="cardNumber" value={this.state.cardNumber} onChange={this.handleChange}/>&nbsp;&nbsp;
-                                    Bank &nbsp;&nbsp;<input  name="bank" value={this.state.bank} onChange={this.handleChange}/>&nbsp;&nbsp;
                                     CCV &nbsp;&nbsp;<input  name="ccv" value={this.state.ccv} onChange={this.handleChange}/><br/><br/>            
                                     Card Holder &nbsp;&nbsp;<input  name="cardHolder" value={this.state.cardHolder} onChange={this.handleChange}/>&nbsp;&nbsp;
                                     EXP &nbsp;&nbsp;<input type="date"  name="exp" value={this.state.exp} onChange={this.handleChange}/>
                                 </div><br/>
                                 <button className="signup_btn modal-btn" onClick={this.toggleModal}>CANCEL</button>
-                                <button className="signup_btn modal-btn" onClick={this.toggleModal}>ADD</button>
+                                <button className="signup_btn modal-btn" onClick={this.handleAdd}>ADD</button>
                             </Modal>
 
                         </div>
                     </div><br></br>
-                <button className="signup_btn" onClick={this.handleSubmit} onChange={this.handleChange}>SIGN UP</button><br></br><br></br>
+                <button className="signup_btn" onClick={this.handleSubmit} >SIGN UP</button><br></br><br></br>
 
-                </form> 
             </div>
             </div>
         );
@@ -255,3 +226,5 @@ class SignUp extends Component {
 
 export default SignUp
 
+
+// expiredate require
