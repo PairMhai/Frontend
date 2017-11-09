ba2b358d661b357129b1af068a320ae6fedaec55
MewBenyapa <ye.amity@gmail.com>
2017-11-09 08:43:14 +0700

    test Promotion

--- src/components/Promotion.js
+++ src/components/Promotion.js
@@ -1,16 +1,25 @@
 import React , {Component} from 'react'
 import Navbar from '../components/Navbar'
-// import LeftTabProfile from '../components/LeftTabFilter'
-// import TabProfile from '../components/LeftTabProfile'
+import LeftTabProfile from '../components/LeftTabProfile'
+import LeftTab from '../components/LeftTab'
 import '../CSS/Payment.css'
 import '../CSS/Promotion.css';
 import axios from 'axios'
+import {Cookies} from 'react-cookie'
 
 class Promotion extends Component {
 
     constructor(props) {
         super(props)
         this.state = { promotions: [] }
+    }
+
+    checkLogin(){
+        const cookies = new Cookies();
+        var key = cookies.get('key');
+        if(key === 'null' || key === undefined)
+            return <LeftTab />;
+        return <LeftTabProfile  />;
     }
 
     componentWillMount() {
@@ -27,9 +36,11 @@ class Promotion extends Component {
     render(){
         const promDet = this.state.promotions.map((promVal, index) => {
             return (
-                <div key={promVal.name}>
-                        <img className="img" src={require('../img/Promotion/'+ promVal.image_name)} width="25%" alt="product pic" />
-                    <div className="promotion-info">
+                <div key={promVal.name} className="row">
+                    <div className="col-sm-4">
+                        <img className="img" src={require('../img/Promotion/'+ promVal.image_name)} width="80%" alt="product pic" />
+                    </div>
+                    <div className="col-sm-6 promotion-info">
                         <h2><div>{promVal.name}</div></h2>                    
                         <div className="promo-detail" name="description">{promVal.description}</div><br/>
                         <div className="promo-detail" name="discount">{promVal.discount}</div><br/>
@@ -44,6 +55,7 @@ class Promotion extends Component {
                 <Navbar /> 
                 <div className="row">
                     <div className="col-md-3 push-md-9">
+                        {this.checkLogin()}
                     </div>
                     <div className="col-md-9 push-md-3 cus-con">
                         <p className="event">SPECIAL EVENT</p>
